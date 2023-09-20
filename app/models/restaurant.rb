5200f6c24740f521e9f1f1d9e2692e678c817983
Dan Lee <danielhlee4@gmail.com>
2023-09-20 11:05:58 -0400

    Add restaurant model validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,31 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id           :bigint           not null, primary key
+#  name         :string           not null
+#  description  :text             not null
+#  cuisine      :string           not null
+#  expense      :string           not null
+#  price_range  :string           not null
+#  neighborhood :string           not null
+#  address      :string           not null
+#  phone        :string           not null
+#  website_url  :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class Restaurant < ApplicationRecord
+    validates :name, :cuisine, :address, :website_url,
+        length: { in: 2..255 }
+    validates :description,
+        length: { in: 3..30000 }
+    validates :expense,
+        inclusion: { in: %w($ $$ $$$ $$$$), message: "%{value} is not a valid expense" }
+    validates :price_range, 
+        inclusion: { in: ["$30 and under", "$31 to $50", "$50 and over"], message: "%{value} is not a valid price range" }
+    validates :neighborhood,
+        inclusion: { in: ["East Village", "Union Square", "Hell's Kitchen"], message: "%{value} is not a valid neighborhood" }
+    validates :phone,
+        inclusion: { is: 14 }
 end
