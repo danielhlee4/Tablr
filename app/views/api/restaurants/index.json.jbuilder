765191c9b78c20ae6e2710ef94133282b12f53bb
Dan Lee <danielhlee4@gmail.com>
2023-09-21 11:30:35 -0400

    add restaurants actions, selectors, thunk action creators

---
+++ app/views/api/restaurants/index.json.jbuilder
@@ -0,0 +1,7 @@
+json.restaurants do
+    @restaurants.each do |restaurant|
+        json.set! restaurant.id do
+            json.extract! restaurant, :id, :name, :description, :cuisine, :expense, :price_range, :neighborhood, :address, :phone, :website_url
+        end
+    end
+end
---
+++ frontend/src/store/restaurants.js
@@ -0,0 +1,49 @@
+// Action constants and their corresponding actions
+export const RECEIVE_RESTAURANTS = 'restaurants/RECEIVE_RESTAURANTS';
+export const RECEIVE_RESTAURANT = 'restaurants/RECEIVE_RESTAURANT';
+
+export const receiveRestaurants = restaurants => {
+    return {
+        type: RECEIVE_RESTAURANTS,
+        restaurants
+    }
+};
+
+export const receiveRestaurant = restaurant => {
+    return {
+        type: RECEIVE_RESTAURANT,
+        restaurant
+    }
+};
+
+// selectors
+export const getRestaurant = restaurantId => {
+    return state => {
+        if (state.restaurants) return state.restaurants[restaurantId];
+        return null; 
+    }
+}
+
+export const getRestaurants = state => {
+    if (state.restaurants) return Object.values(state.restaurants);
+    return [];
+}
+
+//thunk action creators
+export const fetchRestaurants = () => async dispatch => {
+    const res = await fetch('/api/restaurants');
+
+    if (res.ok) {
+        const restaurants = await res.json();
+        dispatch(receiveRestaurants(restaurants));
+    }
+}
+
+export const fetchRestaurant = restaurantId => async dispatch => {
+    const res = await fetch(`/api/restaurants/${restaurantId}`);
+
+    if (res.ok) {
+        const restaurant = await res.json();
+        dispatch(receiveRestaurant(restaurant));
+    }
+}
