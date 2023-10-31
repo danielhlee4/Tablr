5d2d260e23f86a8365f3d1490de47145fcdce149
Dan Lee <danielhlee4@gmail.com>
2023-10-31 13:06:37 -0400

    add past reservation logic

--- frontend/src/components/Reservations/ReservationIndexItem.js
+++ frontend/src/components/Reservations/ReservationIndexItem.js
@@ -2,34 +2,43 @@ import badroman from '../../assets/badroman.webp'
 import './Reservations.css';
 
 function ReservationIndexItem({ reservation }) {
-    function formatDateAndTime(dateStr, timeStr) {
-        // Extract the hour, minute, and second from the timeStr
-        const [_, hours, minutes, seconds] = timeStr.match(/(\d{2}):(\d{2}):(\d{2})/);
     
-        // Combine dateStr with extracted hours, minutes, and seconds, then convert to Date object
-        const combinedDate = new Date(`${dateStr}T${hours}:${minutes}:${seconds}`);
-    
-        const formattedDate = combinedDate.toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' });
-        const formattedTime = combinedDate.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
-    
-        return `${formattedDate} at ${formattedTime}`;
+    function isPastReservation(dateStr, timeStr) {
+        const combinedDate = new Date(`${dateStr}T${timeStr.split('T')[1]}`);
+        const now = new Date();
+        return combinedDate < now;
     }
     
+    function formatDateAndTime(dateStr, timeStr) {
+        const [_, hours, minutes, seconds] = timeStr.match(/(\d{2}):(\d{2}):(\d{2})/);
+        const combinedDate = new Date(`${dateStr}T${hours}:${minutes}:${seconds}`);
+        const formattedDate = combinedDate.toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' });
+        const formattedTime = combinedDate.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
+
+        if (isPastReservation(dateStr, timeStr)) {
+            const currentYear = new Date().getFullYear();
+            const reservationYear = combinedDate.getFullYear();
+            return `${formattedDate}${currentYear !== reservationYear ? `, ${reservationYear}` : ''}`;
+        }
+        
+        return `${formattedDate} at ${formattedTime}`;
+    }
+
     return (
         <div className="index-item-reservations-container">
             <img 
-                // src={reservation.restaurant.photoUrl} 
                 src={badroman}
                 alt={`Image of ${reservation.restaurant.name}`} 
                 className="index-item-reservation-image"
             />
             <div className="index-item-reservation-details">
                 <div className="index-item-reservation-name">{reservation.restaurant.name}</div>
-                <div className="index-item-reservation-status">Reservation confirmed</div>
+                <div className="index-item-reservation-status">{isPastReservation(reservation.date, reservation.time) ? 'Reservation completed' : 'Reservation confirmed'}</div>
                 <div className="index-item-reservation-info">
                     <span>{reservation.partySize} &bull; {formatDateAndTime(reservation.date, reservation.time)}</span>
                 </div>
                 <div>
+                    {isPastReservation(reservation.date, reservation.time) ? <button className="index-item-reservation-action">Leave a review</button> : null}
                     <button className="index-item-reservation-action">Modify</button>
                     <button className="index-item-reservation-action">Cancel</button>
                 </div>
